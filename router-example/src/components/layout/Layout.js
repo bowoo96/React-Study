b54d9d0076cce38022f9af53fcb7292049d0da63
bowoo96 <qhdn131@naver.com>
2022-06-10 13:25:06 +0900

    Add : 인용문 리스트 JSX 추가

--- router-example/src/components/layout/Layout.js
+++ router-example/src/components/layout/Layout.js
@@ -1,5 +1,15 @@
-const Layout = () => {
-    return;
+import { Fragment } from "react/cjs/react.production.min";
+
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
+
+const Layout = (props) => {
+    return (
+        <Fragment>
+            <MainNavigation />
+            <main className={classes.main}>{props.children}</main>
+        </Fragment>
+    );
 };
 
 export default Layout;
