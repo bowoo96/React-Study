54ddfa6db0a823768bf9834231538ec68f7ea38b
bowoo96 <qhdn131@naver.com>
2022-06-10 19:20:30 +0900

    Feat : router 관련 기능 추가
    - URL를 다르게 입력 했을 때 에러 페이지 출력 기능
    - NewQuote로 quote 내용 입력하고 페이지 이동할 때 경고창 출력 기능
    - NewQuote에서 Add 버튼 클릭 시 /quotes로 URL 이동

--- router-example/src/pages/QuoteDetail.js
+++ router-example/src/pages/QuoteDetail.js
@@ -1,16 +1,35 @@
 import { useParams, Route } from "react-router-dom";
-import { Fragment } from "react/cjs/react.production.min";
+import { Fragment } from "react";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
 
-const QuoteDefail = () => {
+const DUMMY_QUOTES = [
+    {
+        id: "q1",
+        author: "Max",
+        text: "Learning React is fun!",
+    },
+    {
+        id: "q2",
+        author: "bowoo",
+        text: "Learning React is great!",
+    },
+];
+
+const QuoteDetail = () => {
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
     console.log(params.quoteId);
+    if (!quote) {
+        return <p>No quote found!</p>;
+    }
+
     return (
         <Fragment>
-            <h1>Quote Detail Page</h1>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
@@ -18,4 +37,4 @@ const QuoteDefail = () => {
     );
 };
 
-export default QuoteDefail;
+export default QuoteDetail;
